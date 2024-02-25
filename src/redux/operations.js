562c45b8f36d9f4acba983ed5d5f37721057c67f
OnishYurii <onishchenkoyurii@gmail.com>
2024-02-25 18:03:55 +0200

    deleteContact done

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -12,11 +12,23 @@ export const fetchContacts = createAsyncThunk('contacts/fetchall', async (_, thu
   }
 });
 
-export const addContact = createAsyncThunk('contacts/addContact', async (values, thinkAPI) => {
+export const addContact = createAsyncThunk('contacts/addContact', async (values, thunkAPI) => {
   try {
     const response = await axios.post('/contacts', values);
     return response.data;
   } catch (er) {
-    return thinkAPI.rejectWithValue(er.message);
+    return thunkAPI.rejectWithValue(er.message);
   }
 });
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactID, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactID}`);
+      return response.data;
+    } catch (er) {
+      return thunkAPI.rejectWithValue(er.message);
+    }
+  }
+);
